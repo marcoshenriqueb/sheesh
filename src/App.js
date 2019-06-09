af10c6f039d87aad6a05c7f05362b499ea7f5b86
Marcos Almeida <marcoshenriqueb@gmail.com>
2019-06-09 20:47:26 -0300

    Muitas mudanças rs

--- src/App.js
+++ src/App.js
@@ -2,42 +2,41 @@ import React from 'react';
 import './App.scss';
 import Logo from './assets/img/logo.png';
 import Burger1 from './assets/img/lifestyle1.jpg';
-import Burger3 from './assets/img/burger2.jpg';
+import Burger from './assets/img/burger1.jpg';
 
 const App = () => (
   <div className="App">
+    <div className="info">
+      <span>Seg - Dom: 12h - 24h</span>
+      <span>Rua Oliveira Fausto n29A, Botafogo</span>
+      <span>(21) 4116-1207</span>
+    </div>
     <header className="header">
       <img className="Logo" src={Logo} alt="Sheesh" />
-      <nav className="nav">
-        <a href="#franquia" className="nav-item">Vire um franqueado!</a>
+      <span>Menu</span>
+      <nav className="menu">
+        <div className="close-btn__container">
+          <span className="close-btn">X</span>
+        </div>
+        <div className="nav">
+          <a href="#franquia" className="nav-item">.home</a>
+          <a href="#franquia" className="nav-item">.o porquê</a>
+          <a href="#franquia" className="nav-item">.cardápio</a>
+          <a href="#franquia" className="nav-item">.contato</a>
+        </div>
       </nav>
     </header>
-    <section className="header-image">
-      <h2 className="title title-small">Siga as nossas redes</h2>
-      <div>
-        <a
-          href="https://www.instagram.com/sheesh.burger/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Social-anchor"
-        >
-          <i className="fab fa-instagram" />
-        </a>
-        &nbsp;&nbsp;&nbsp;
-        <a
-          href="https://www.facebook.com/sheeshburger/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Social-anchor"
-        >
-          <i className="fab fa-facebook-square" />
-        </a>
+    <section className="home">
+      <div className="pop-bg">
+        <h2 className="title">É xix que fala?</h2>
+        <button type="button" className="button">Cardápio</button>
       </div>
-      <span className="small primary-font">rua oliveira fausto 29a, botafogo, rj</span>
-      <span className="small primary-font">(21) 4116-1207</span>
+      <div className="header-image">
+        &nbsp;
+      </div>
     </section>
     <section className="App-section Primary-bg">
-      <h2 className="title">Por quê?</h2>
+      <h2 className="subtitle">.o porquê</h2>
       <p className="storystelling sheesh-border border-padding">
         O SHEESH! foi criado com o propósito de oferecer um hambúrguer artesanal
         de qualidade, por um preço justo. Fazemos uma escolha criteriosa de cada
@@ -48,14 +47,36 @@ const App = () => (
         Venha conhecer o nosso cantinho em Botafogo! Ahh, não esqueça de pronunciar
         certinho, é Xix que fala ;)
       </p>
-      <div className="image-container">
-        <img className="image" src={Burger1} alt="Sheesh" />
-        <img className="image" src={Burger3} alt="Sheesh" />
-      </div>
+      <img className="image" src={Burger1} alt="Sheesh" />
     </section>
     <section className="App-section Primary-bg">
-      <h2 className="title">Nossa loja</h2>
-      <p>http://neptunian.github.io/react-photo-gallery/</p>
+      <h2 className="subtitle">.cardápio</h2>
+      <div className="food-menu">
+        <div className="food-menu__item">
+          <span className="food-menu__item-title">Burgers</span>
+          <div className="food-menu-image">
+            <img className="image" src={Burger} alt="Sheesh" />
+          </div>
+        </div>
+        <div className="food-menu__item">
+          <span className="food-menu__item-title">Appetizers</span>
+          <div className="food-menu-image">
+            <img className="image" src={Burger} alt="Sheesh" />
+          </div>
+        </div>
+        <div className="food-menu__item">
+          <span className="food-menu__item-title">Sauces</span>
+          <div className="food-menu-image">
+            <img className="image" src={Burger} alt="Sheesh" />
+          </div>
+        </div>
+        <div className="food-menu__item">
+          <span className="food-menu__item-title">Sweet Treats</span>
+          <div className="food-menu-image">
+            <img className="image" src={Burger} alt="Sheesh" />
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 );
